632f4efffe982b8fabee73156f48542f55d9f575
mohammedsheheer <mohammed.sheheer2@gmail.com>
2025-12-15 11:15:51 +0530

    Add password validation

--- student_registeration.py
+++ student_registeration.py
@@ -1,4 +1,17 @@
 name = input("Enter your name: ")
 pwd = input("Enter your password: ")
 
-print("Student registeration successful!")
+#password validation
+def validate_password(password):
+    if len(password) < 8:
+        return False
+    if not any(char.isdigit() for char in password):
+        return False
+    if not any(char.isalpha() for char in password):
+        return False
+    return True
+
+if(validate_password):
+    print("Student registeration successful!")
+else:
+    print("Please enter a strong password!")
